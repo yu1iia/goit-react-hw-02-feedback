2366774a4cc113413c7741a07948c272817916cd
Yuliia Burko <yuliia.burko@gmail.com>
2021-06-07 22:34:44 +0200

    added initial files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Feedback from './components/Feedback/Feedback';
+
+/* import user from './json/user.json';
+import stats from './json/statistical-data.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json'; */
+
+const App = () => {
+  return (
+    <div>
+      <Feedback />
+    </div>
+  );
+};
+
+export default App;
